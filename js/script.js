1b5cc89efa790f45f5b179d42bb2a6719f1f4d0e
xmnnasrul <mygithub.nasrul@gmail.com>
2023-03-02 11:22:39 +0700

    add animation

--- js/script.js
+++ js/script.js
@@ -10,3 +10,34 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+function toggleNavbar() {
+  let navbar = document.querySelector(".navbar");
+  if (window.innerWidth > 768) {
+    // hanya jalankan pada tampilan desktop
+    let prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+      let currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        // Scroll up
+        navbar.classList.remove("hide-nav");
+      } else {
+        // Scroll down
+        navbar.classList.add("hide-nav");
+      }
+      prevScrollpos = currentScrollPos;
+    };
+  } else {
+    // nonaktifkan saat tampilan mobile
+    navbar.classList.remove("hide-nav");
+    window.onscroll = null;
+  }
+}
+
+toggleNavbar();
+
+window.addEventListener("resize", function () {
+  toggleNavbar();
+});
+
+TweenMax.to(".hu__hu__", 1, { y: 20, yoyo: true, repeat: -1, ease: Sine.easeInOut });
